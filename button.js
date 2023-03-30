35e86e9377602e83f38fd512a8ee66da6ab74431
Highgore12 <erikssonliv841@gmail.com>
2023-03-30 11:19:57 +0200

    Fixed some stuff

--- button.js
+++ button.js
@@ -1,18 +1,11 @@
 let siffra = 0;
 
 function plusclick(){
-    siffra = siffra + 1;
+    siffra = siffra +1;
     document.getElementById("tal").innerHTML = siffra;
 
 }
 function minusclick(){
     siffra = siffra -1;
-    document.getElementById("tal").innerHTML = siffra;
-}
-const minus = document.getElementById("minus");
-minus.addEventListener("click", minus);
-
-function minus(){
-    siffra = siffra - 1;
     document.getElementById("tal").innerHTML = siffra;
 }
