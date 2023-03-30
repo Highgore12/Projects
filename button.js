d1a5c9904057017d407a80e43cc9ffda24623029
Highgore12 <erikssonliv841@gmail.com>
2023-03-30 11:16:43 +0200

    Done some stuff

--- button.js
+++ button.js
@@ -2,5 +2,17 @@ let siffra = 0;
 
 function plusclick(){
     siffra = siffra + 1;
-    document.getElementById("tal").innerHTML = siffra
+    document.getElementById("tal").innerHTML = siffra;
+
+}
+function minusclick(){
+    siffra = siffra -1;
+    document.getElementById("tal").innerHTML = siffra;
+}
+const minus = document.getElementById("minus");
+minus.addEventListener("click", minus);
+
+function minus(){
+    siffra = siffra - 1;
+    document.getElementById("tal").innerHTML = siffra;
 }
